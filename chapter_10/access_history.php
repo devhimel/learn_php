3810e964ffcf94b0811ddbb988639e19680a98c0
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-10-21 01:42:16 +0600

    access_history.php file added

--- chapter_10/access_history.php
+++ chapter_10/access_history.php
@@ -1,2 +1,11 @@
 <?php
 $file = "userAvatar.jpg";
+if(file_exists($file)) {
+    $format = "D d M y g:i A";
+    $atime = fileatime($file);
+    $mtime = filemtime($file);
+    $ctime = filectime($file);
+    echo "Last accessed: " . date($format, $atime) . "<br>";
+    echo "Last modified: " . date($format, $mtime) . "<br>";
+    echo "Last changed: " . date($format, $ctime);
+}
