9533c4693c36ceb8f18f1a4f3f0a6276caf9a7ae
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-10-22 13:42:56 +0600

    readfile02.php file added

---
+++ file_handling/readfile02.php
@@ -0,0 +1,13 @@
+<?php
+
+$file = "userAvatar.jpg";
+if(file_exists($file)){
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment: filename="'.basename($file).'"');
+    header('Expaires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length:' . filesize($file));
+    readfile($file);
+}
